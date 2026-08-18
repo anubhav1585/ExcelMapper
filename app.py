db9d4fc4b742067b32d9518a818d615b084061b2
Anubhav Pal <anubhav1585@gmail.com>
2026-08-18 11:02:23 +0530

    UI development (V2)

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from   flask import Flask
 from flask import render_template
+from flask import request
 
 Excel_Mapper_app = Flask(__name__)
 
-@Excel_Mapper_app.route('/')
+@Excel_Mapper_app.route('/',methods=["GET", "POST"])
 
 def Excel_Mapper():
+    if request.method == "POST":
+        print("Post request received")
     return render_template("Excel_mapper.html")
 
 
 
 if __name__ == "__main__":
     Excel_Mapper_app.run(debug=True)
-    
